0b4f1748613d60282284d66c063045a76157c321
Alan Antony James <s10207327@connect.np.edu.sg>
2022-01-15 19:58:51 +0800

    Initial Commit

---
+++ Eventsly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eventsly
+//
+//  Created by MAD2 on 15/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
