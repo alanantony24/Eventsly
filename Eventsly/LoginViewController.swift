5dfb372087487277a5cc45a7e56ac70e1201781f
Keith Toh <keith2703@icloud.com>
2022-01-20 16:38:39 +0800

    added viewControllers in home storyboard

--- Eventsly/LoginViewController.swift
+++ Eventsly/LoginViewController.swift
@@ -14,5 +14,12 @@ class LoginViewController:UIViewController{
         super.viewDidLoad()
     }
     
+    // for testing
+    @IBAction func loginBtn(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "Content") as UIViewController
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+    }
 }
 
--- Eventsly/ProfileViewController.swift
+++ Eventsly/ProfileViewController.swift
@@ -1,17 +1,17 @@
 //
-//  HomeViewController.swift
+//  ProfileViewController.swift
 //  Eventsly
 //
-//  Created by MAD2 on 18/1/22.
+//  Created by Keith Toh on 20/1/22.
 //
 
 import Foundation
 import UIKit
 
-class HomeViewController: UIViewController {
+class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
 }
