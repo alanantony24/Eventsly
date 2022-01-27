97576f52dfb3b67f9c1e0d76791e8f74d20a8f70
Alan Antony James <s10207327@connect.np.edu.sg>
2022-01-27 19:01:26 +0800

    Implemented location controller.

---
+++ Eventsly/LocationDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  LocationDelegate.swift
+//  Eventsly
+//
+//  Created by MAD2 on 27/1/22.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationDelegate: NSObject, CLLocationManagerDelegate{
+    var locationCallback: ((CLLocation) -> ())? = nil
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation])
+    {
+        
+    guard let currentLocation = locations.last else { return }
+    locationCallback?(currentLocation)
+        
+    }
+}
---
+++ Eventsly/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  Eventsly
+//
+//  Created by MAD2 on 27/1/22.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
