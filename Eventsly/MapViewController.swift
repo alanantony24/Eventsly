dd517b975dbf1f9e57c5c8acc3fef3ca4dd13631
Alan Antony James <s10207327@connect.np.edu.sg>
2022-01-27 22:21:51 +0800

    implemented showing the current user location on map

--- Eventsly/MapViewController.swift
+++ Eventsly/MapViewController.swift
@@ -11,8 +11,48 @@ import MapKit
 
 class MapViewController:UIViewController{
     
+    @IBOutlet weak var map: MKMapView!
+    
+    let regionRadius:CLLocationDistance = 1000
+    
+    func centerMapOnLocation(location:CLLocation){
+        let coordinateRegion = MKCoordinateRegion(
+            center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius
+        )
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    let annotation = MKPointAnnotation()
+    
+    let locationDelegate = LocationDelegate()
+    
+    var latestLocation:CLLocation? = nil
+    
+    let locationManager:CLLocationManager = {
+        $0.requestAlwaysAuthorization()
+        $0.desiredAccuracy = kCLLocationAccuracyBest
+        $0.startUpdatingLocation()
+        $0.startUpdatingHeading()
+        return $0
+    }(CLLocationManager())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = locationDelegate
+        locationDelegate.locationCallback = { location in
+            self.latestLocation = location
+            let lat = String(format: "Lat: %3.8f", location.coordinate.latitude)
+            let long = String(format: "Long: %3.8f", location.coordinate.longitude)
+            print("\(lat), \(long)")
+            
+            self.centerMapOnLocation(location: location)
+            self.annotation.coordinate = location.coordinate
+            self.annotation.title = "Me"
+            self.annotation.subtitle = "My current location"
+            self.map.addAnnotation(self.annotation)
+            self.map.showsUserLocation = true
+        }
     }
     
 }
